c591a125b352a883e7561d151c636b5ed782eddd
Marius Anderie <marius.anderie@hotmail.com>
2018-04-05 02:32:09 +0200

    added some shellcode generation

---
+++ ShellcodeGenerator/System.c
@@ -0,0 +1,12 @@
+int main() {
+    system("echo hi>hack.txt");
+    // asm("push ebp;"
+    //     "mov ebp, esp;"
+    //     "push offset aEchoHiHack_txt;"
+    //     "call sub_402567;"
+    //     "add esp, 4;"
+    //     "xor eax, eax;"
+    //     "pop ebp;"
+    //     "_data   segment para public 'DATA' use32"
+    // );
+}
