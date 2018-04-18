fce8b303ef0bc778273cba77a2c05e70260baf89
Marius Anderie <marius.anderie@hotmail.com>
2018-04-18 22:26:01 +0200

    added GetProcessByNameEx

--- MemWarsServices/TestPivotApp.c
+++ MemWarsServices/TestPivotApp.c
@@ -13,7 +13,7 @@ int main(int argc, char* argv[]) {
 	ZeroMemory(&pi, sizeof(pi));
 	CreateProcess(
         TEXT("memoryTestApp.exe"),
-        NULL,NULL,NULL,TRUE,
+        NULL,NULL,NULL,FALSE,
         0,
         NULL,NULL,
         &si,
@@ -23,11 +23,11 @@ int main(int argc, char* argv[]) {
     HANDLE process;
     for (;;) {
         Sleep(1000);
-        process = (HANDLE)GetProcessByName(TEXT("memoryTestApp.exe"));
+        process = (HANDLE)GetProcessByNameEx(TEXT("memoryTestApp.exe"), FALSE, PROCESS_QUERY_INFORMATION);
         if (process == NULL) {
             CreateProcess(
                 TEXT("memoryTestApp.exe"),
-                NULL,NULL,NULL,TRUE,
+                NULL,NULL,NULL,FALSE,
                 0,
                 NULL,NULL,
                 &si,
