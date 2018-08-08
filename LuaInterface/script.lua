90700d5cae0ef14b54436a1872580e54f9620a49
Marius <marius.anderie@hotmail.com>
2018-08-08 14:36:59 +0200

    small additions

--- LuaInterface/script.lua
+++ LuaInterface/script.lua
@@ -8,8 +8,17 @@ if useDllInjector then
     injector:SetTargetDLL("C:/Users/Marius/git/MemWars/AttackServices/DLLInjectionAttack/InjectedDLL.dll")
     injector:SetTargetProcessByName("notepad.exe")
     if injector:InjectDLL() then
-        print("success")
+        print("Successfully injected the DLL in the target process")
     end
     print(injector:GetAttackResults())
 end
 
+if useDirect3DInjector then
+    direct3dinjector = DLLInjector()
+    direct3dinjector:SetTargetDLL("C:/Users/Marius/git/MemWars/AttackServices/Direct3D11HookAttack/Direct3DHookDLL.dll")
+    direct3dinjector:SetTargetProcessByName("Direct3DTestApp.exe")
+    if direct3dinjector:InjectDLL() then
+        print("Successfully hooked the Direct 3D Function PresentHook()")
+    end
+    print(direct3dinjector:GetAttackResults())
+end
